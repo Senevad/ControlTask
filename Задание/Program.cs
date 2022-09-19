e88c47aef5e169f0bded1fbc788e7a59f1c15a11
Senchenko_Vadim <Senevad@yandex.ru>
2022-09-19 03:03:49 +0300

    дописал программу

--- Задание/Program.cs
+++ Задание/Program.cs
@@ -1,8 +1,27 @@
 ﻿Console.Clear();
 
 Console.WriteLine("Введите 5 строк");
+int count = 0;
 string [] stringArray = new string [5];
 for (int i = 0; i < stringArray.GetLength(0); i++)
 {
     stringArray[i] = Console.ReadLine()!;
+    if (stringArray[i].Length <= 3)
+    {
+        count++;
+    }
 }
+
+string [] needStringArray = new string [count];
+int temp = 0;
+for (int i = 0; i < stringArray.GetLength(0); i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        needStringArray[temp] = stringArray[i];
+        temp++;
+    }
+}
+
+    Console.Write($"Итоговый Массив: {String.Join(",",needStringArray)}");
+
